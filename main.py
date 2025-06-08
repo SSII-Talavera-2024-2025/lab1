059105df79dfae1d0269d7d863907d7765390d7b
notlurcky <Daniel.tomas1@alu.uclm.es>
2025-06-08 23:15:29 +0200

    un crear grafo.py y otro main para ejecutarlo que lo saca en txt

---
+++ main.py
@@ -0,0 +1,24 @@
+from creargrafo import parse_graphml
+
+if __name__ == "__main__":
+    # Cambia esto por el nombre de tu archivo
+    file_path = 'CR_Capital.graphml'
+    nodes, edges, adjacency_list = parse_graphml(file_path)
+
+    with open("prueba.txt", "w", encoding="utf-8") as f:
+        f.write("NODOS:\n")
+        for node_id, info in nodes.items():
+            f.write(f"{node_id}: OSMID={info['osmid']}, Lon={info['lon']}, Lat={info['lat']}\n")
+
+        f.write("\nARISTAS:\n")
+        for edge in edges:
+            f.write(f"{edge['source']} -> {edge['target']} (Longitud: {edge['length']})\n")
+
+        f.write("\nLISTA DE ADYACENCIA:\n")
+        for node, neighbors in adjacency_list.items():
+            f.write(f"{node}: {neighbors}\n")
+
+
+
+
+        
